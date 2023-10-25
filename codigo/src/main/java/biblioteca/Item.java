efc9250c6e473fb19b0646c61fa4c6f73cb98713
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2023-10-25 11:26:50 -0300

    att

--- codigo/src/main/java/biblioteca/Item.java
+++ codigo/src/main/java/biblioteca/Item.java
@@ -79,36 +79,3 @@ public abstract class Item implements Comparable<Item>, Emprestavel {
     }
 }
 
-public interface Itens {
-    pegarItem();
-
-    devolverItem();
-}
-
-public class Livro extends Item implements Itens {
-    public Livro(String titulo, String autor, int ano, boolean emprestavel, int quantidade) {
-        super(titulo, autor, ano, emprestavel, quantidade);
-    }
-
-    public Usuario(String nome) {
-        this.nome = nome;
-    }
-
-    public void pegarItem(Item item) {
-        if (!emprestimoAtrasado && itensEmprestados.size() < 3 && item.podeSerEmprestado()) {
-            itensEmprestados.add(item);
-            item.setQuantidade(item.getQuantidade() - 1);
-        } else {
-            System.out.println("Não foi possível pegar o item.");
-        }
-    }
-
-    public void devolverItem(Item item) {
-        if (itensEmprestados.contains(item)) {
-            itensEmprestados.remove(item);
-            item.setQuantidade(item.getQuantidade() + 1);
-        } else {
-            System.out.println("O item não foi emprestado por este usuário.");
-        }
-    }
-}
